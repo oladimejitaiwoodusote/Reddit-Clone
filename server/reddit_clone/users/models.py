a7bed9614d0654fd16b82f8313050fd1e0df390a
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2025-01-27 18:46:05 +0100

    users v0

---
+++ server/reddit_clone/users/models.py
@@ -0,0 +1,8 @@
+from reddit_clone import db
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key= True)
+    
--- server/test_dotenv.py
+++
@@ -1,4 +0,0 @@
-from dotenv import dotenv_values
-from dotenv import load_dotenv
-
-import os
