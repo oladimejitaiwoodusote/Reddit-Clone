e35001149aa452533c0663832eb9f5a28b0f78dd
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2025-01-28 18:04:12 +0100

    users v1

--- server/reddit_clone/users/models.py
+++ server/reddit_clone/users/models.py
@@ -1,8 +1,61 @@
 from reddit_clone import db
+from flask import jsonify
+from flask_bcrypt import Bcrypt
 
+#Model and Methods
 class User(db.Model):
 
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key= True)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    full_name = db.Column(db.String, nullable = False)
+    username = db.Column(db.String(80), unique =True, nullable = False)
+    password_hash = db.Column(db.Text, nullable = False)
+    default_avatar_url = "https://storage.googleapis.com/instagram-clone/Empty%20Avatar.jpeg"
+    avatar = db.Column (db.String, default= default_avatar_url)
+    bio = db.Column (db.Text)
+
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, server_default = db.func.now(), onupdate = db.func.now())
+
+    #methods needed
+    #Get user profile
+    #create user account
+    #update/patch user account
+    #delete user account
+    #login 
+    #logout
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def to_dict(self):
+        return (
+            {
+                "email": self.email,
+                "full_name": self.full_name,
+                "username": self.username,
+                "default_avatar_url": self.default_avatar_url,
+                "avatar": self.avatar,
+                "bio": self.bio
+            }
+        )
     
+    #Get User
+    def get_user(self, id):
+        user = User.query.get(id)
+        if user:
+            return jsonify(user.to_dict())
+
+    #Create User
+    @classmethod
+    def create_user(cls, form_data):
+        new_user = User(
+            email = form_data["email"],
+            full_name = form_data["full_name"],
+            username= form_data["username"],
+            password_hash = 2
+ 
+        )
