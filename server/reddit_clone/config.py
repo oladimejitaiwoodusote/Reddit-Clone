3695be269bbad0be31986e42bec4cf10d366d717
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2025-12-01 15:54:23 +0100

    Deployment fix v0

--- server/reddit_clone/config.py
+++ server/reddit_clone/config.py
@@ -1,13 +1,16 @@
 from dotenv import dotenv_values
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-DATABASE_URI = dotenv_values()["DATABASE_URI"]
-SECRET_KEY = dotenv_values()["SECRET_KEY"]
+DATABASE_URI = os.environ.get("DATABASE_URI")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 SESSION_COOKIE_HTTPONLY = True
 SESSION_COOKIE_SECURE = False  # change to True in production w/ HTTPS
 SESSION_COOKIE_SAMESITE = "Lax"
 
 # Cloudinary credentials
-CLOUDINARY_CLOUD_NAME = dotenv_values()["CLOUDINARY_CLOUD_NAME"]
-CLOUDINARY_API_KEY = dotenv_values()["CLOUDINARY_API_KEY"]
-CLOUDINARY_API_SECRET = dotenv_values()["CLOUDINARY_API_SECRET"]
+CLOUDINARY_CLOUD_NAME = os.environ.get("CLOUDINARY_CLOUD_NAME")
+CLOUDINARY_API_KEY = os.environ.get("CLOUDINARY_API_KEY")
+CLOUDINARY_API_SECRET = os.environ.get("CLOUDINARY_API_SECRET")
